1ded92ef415e490f76d0c3ed65f84a9b12adc509
DESKTOP-V7U6PIA\Oat <manassanan.bo>
2017-04-24 22:21:51 +0700

    form fill color

--- assets/js/webJs.js
+++ assets/js/webJs.js
@@ -5,12 +5,91 @@
       name:"",
       age:"",
       gender:"M",
-      disease:[]
+      disease:[],
+      relativePatient:{
+      	patient:{
+      		ySis:"",oSis:"",yBro:"",oBro:""
+      	},
+      	father:{
+      		ySis:"",oSis:"",yBro:"",oBro:""
+      	},
+      	mother:{
+      		ySis:"",oSis:"",yBro:"",oBro:""
+      	}
+      },
+      attr1Style:{
+      	width:'75px', height:'75px', float:'left' ,backgroundColor:'white'
+      },
+      attr2Style:{
+      	width:'75px', height:'75px', float:'left' ,backgroundColor:'white'
+      },
+      attr3Style:{
+      	width:'75px', height:'75px', float:'left' ,backgroundColor:'white'
+      },
+      attr4Style:{
+      	width:'75px', height:'75px', float:'left' ,backgroundColor:'white'
+      }
     },
     created(){
 
     },
     methods:{
+	    attrFill:function(a) {
+	        switch (a) {
+	            case "A":
+	                return "#0000FE";break;
+	            case "B":
+	                return "#FF00FF";break;
+	            case "C":
+	                return "#FE0000";break;
+	            case "D":
+	                return "#C00000";break;
+	            case "E":
+	                return "#C00000";break;
+	            case "F":
+	                return "#FFC000";break;
+	            case "G":
+	                return "#01FF00";break;
+	            case "H":
+	                return "#800080";break;
+	            case "I":
+	                return "#939FBB";break;
+	            case "J":
+	                return "#01FFFF";break;
+	            case "K":
+	                return "#359AFF";break;
+	            case "L":
+	                return "#FFFF00";break;
+	            case "S":
+	                return "red";
+	            default:
+	                return "transparent";
+	        }
+        },
+        fillColorForm:function(){
+        	if(this.disease.length==0){
+        		this.attr1Style.backgroundColor = 'white'
+        		this.attr2Style.backgroundColor = 'white'
+        		this.attr3Style.backgroundColor = 'white'
+        		this.attr4Style.backgroundColor = 'white'
+        		return
+        	}
+        	if(this.disease.length>0){
+        		this.attr1Style.backgroundColor = this.attrFill(this.disease[0])
+        		this.attr2Style.backgroundColor = 'white'
+        		this.attr3Style.backgroundColor = 'white'
+        		this.attr4Style.backgroundColor = 'white'
+        	}if(this.disease.length>1){
+        		this.attr2Style.backgroundColor = this.attrFill(this.disease[1])
+        		this.attr3Style.backgroundColor = 'white'
+        		this.attr4Style.backgroundColor = 'white'
+        	}if(this.disease.length>2){
+        		this.attr3Style.backgroundColor = this.attrFill(this.disease[2])
+        		this.attr4Style.backgroundColor = 'white'
+        	}if(this.disease.length>3){
+        		this.attr4Style.backgroundColor = this.attrFill(this.disease[3])
+        	}
+        }
 
     },
     mounted(){
